cfc6b72163916f469ded996d5c1d22891d19eb63
guitischer <guitischer@hotmail.com.br>
2022-09-11 23:11:06 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,13 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello World! 🔥</Text>
+      <Button title="Botão" />
       <StatusBar style="auto" />
     </View>
   );
+}
+
+interface ButtonProps {
+  title: string
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
 }
 
 const styles = StyleSheet.create({

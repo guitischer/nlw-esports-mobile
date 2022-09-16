8183c02dcce50bb27569e6f2e86fc824dfb648d1
guitischer <guitischer@hotmail.com.br>
2022-09-15 23:00:35 -0300

    added fetch games and removed unused files

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,36 +1,39 @@
-import { View, Image, FlatList } from 'react-native';
+import { useEffect, useState } from "react";
+import { View, Image, FlatList } from "react-native";
 
-import logoImg from '../../assets/logo-nlw-esports.png';
-import { GameCard } from '../../components/GameCard';
-import { Heading } from '../../components/Heading';
-import { GAMES } from '../../utils/games';
+import logoImg from "../../assets/logo-nlw-esports.png";
 
-import { styles } from './styles';
+import { GameCard, GameCardProps } from "../../components/GameCard";
+import { Heading } from "../../components/Heading";
+
+import { styles } from "./styles";
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect(() => {
+    fetch("http://192.168.1.6:3333/games")
+      .then((response) => response.json())
+      .then((data) => setGames(data));
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Image
-        source={logoImg}
-        style={styles.logo} />
+      <Image source={logoImg} style={styles.logo} />
 
       <Heading
-        title='Encontre seu duo!'
-        subtitle='Selecione o game que deseja jogar...'
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
       />
 
       <FlatList
         contentContainerStyle={styles.contentList}
-        data={GAMES}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => (
-          <GameCard data={item} />
-        )}
+        data={games}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
         horizontal
         showsHorizontalScrollIndicator={false}
       />
-
-
     </View>
   );
 }

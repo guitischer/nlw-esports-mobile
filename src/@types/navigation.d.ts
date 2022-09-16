2dbc8535af00c845b9e40c6453cd161ffe32f2b9
guitischer <guitischer@hotmail.com.br>
2022-09-16 01:09:00 -0300

    updated game screen and route

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -1,13 +1,13 @@
+export interface GameParams{
+  id: string;
+  title: string;
+  bannerUrl: string;
+}
 export declare global {
   namespace ReactNavigation {
     interface RootParamList{
-      home:undefined;
-      game:
-      {
-        id: string;
-        title:string;
-      bannerUrl: string
-      }
+      home: undefined;
+      game: GameParams
     }
   }
 }

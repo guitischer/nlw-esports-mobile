dda22446bfd10624deb8d68c7e226da72f01f8e7
guitischer <guitischer@hotmail.com.br>
2022-09-16 21:28:07 -0300

    added notifications structure

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { StatusBar } from "react-native";
+import { useRef, useEffect } from "react";
 import {
   useFonts,
   Inter_400Regular,
@@ -6,9 +7,15 @@ import {
   Inter_700Bold,
   Inter_900Black,
 } from "@expo-google-fonts/inter";
+import { Subscription } from "expo-modules-core";
+
 import { Background } from "./src/components/Background";
 import { Routes } from "./src/routes";
 import { Loading } from "./src/components/Loading";
+
+import * as Notifications from "expo-notifications";
+import "./src/services/notificationConfigs";
+import { getPushNotificationToken } from "./src/services/getPushNotificationToken";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -17,6 +24,39 @@ export default function App() {
     Inter_700Bold,
     Inter_900Black,
   });
+
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  }, []);
+
+  useEffect(() => {
+    getNotificationListener.current =
+      Notifications.addNotificationResponseReceivedListener((notification) => {
+        console.log(notification);
+      });
+
+    responseNotificationListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      if (
+        getNotificationListener.current &&
+        responseNotificationListener.current
+      ) {
+        Notifications.removeNotificationSubscription(
+          getNotificationListener.current
+        );
+        Notifications.removeNotificationSubscription(
+          responseNotificationListener.current
+        );
+      }
+    };
+  }, []);
 
   return (
     <Background>
